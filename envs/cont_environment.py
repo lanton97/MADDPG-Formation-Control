a2a9a1083e955ed7965e07bcb04632bdefaca16d
Luka Antonyshyn <15la@caslab.queensu.ca>
2021-03-24 22:51:35 -0400

    Cleanup init and add position to info

--- envs/cont_environment.py
+++ envs/cont_environment.py
@@ -18,30 +18,14 @@ class ContMultiAgentEnv(MultiAgentEnv):
                  observation_callback=None, info_callback=None,
                  done_callback=None, shared_viewer=True):
 
-        self.world = world
-        self.agents = self.world.policy_agents
-        # set required vectorized gym env property
-        self.n = len(world.policy_agents)
-        # scenario callbacks
-        self.reset_callback = reset_callback
-        self.reward_callback = reward_callback
-        self.observation_callback = observation_callback
-        self.info_callback = info_callback
-        self.done_callback = done_callback
-        # environment parameters
-        self.discrete_action_space = False
-        # if true, action is a number 0...N, otherwise action is a one-hot N-dimensional vector
-        # This is the 
-        self.discrete_action_input = False
-        # if true, even the action is continuous, action will be performed discretely
-        self.force_discrete_action = False#world.discrete_action if hasattr(world, 'discrete_action') else False
-        # if true, every agent has the same reward
-        self.shared_reward = world.collaborative if hasattr(world, 'collaborative') else False
-        self.time = 0
+        super().__init__(world, reset_callback, reward_callback, observation_callback, 
+                info_callback, done_callback, shared_viewer)
 
-        # configure spaces
+        self.discrete_action_input = False
+        self.force_discrete_action = False        # configure spaces
         self.action_space = []
         self.observation_space = []
+
         for agent in self.agents:
             total_action_space = []
             u_action_space = spaces.Box(low=-agent.u_range, high=+agent.u_range, shape=(world.dim_p,), dtype=np.float32)
@@ -62,11 +46,13 @@ class ContMultiAgentEnv(MultiAgentEnv):
             self.observation_space.append(spaces.Box(low=-np.inf, high=+np.inf, shape=(obs_dim,), dtype=np.float32))
             agent.action.c = np.zeros(self.world.dim_c)
 
-        # rendering
-        self.shared_viewer = shared_viewer
-        if self.shared_viewer:
-            self.viewers = [None]
-        else:
-            self.viewers = [None] * self.n
         self._reset_render()
 
+
+    # We override the 
+    def step(self, action_n):
+        obs_n, reward_n, done_n, info_n = super().step(action_n)
+        for agent in self.agents:
+            info_n.append(agents.state.p_pos)
+
+        return obs_n, reward_n, done_n, info_n
