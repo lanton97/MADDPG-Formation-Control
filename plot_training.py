620946ffa7176be6293ff05d8145059a256cab9b
Luka Antonyshyn <15la@caslab.queensu.ca>
2021-04-22 13:55:41 -0400

    Plotting update and remove old file

--- plot_training.py
+++ plot_training.py
@@ -8,32 +8,30 @@ parser = argparse.ArgumentParser(description='File to plot training data.')
 
 parser.add_argument('--avg-files', dest='avg_file_paths', nargs='+', default=[],
                     help='Files which hold the average training data')
-parser.add_argument('--avg-labels', dest='avg_labels', nargs='+', default=[],
-                    help='Labels for the avergae training data.')
+parser.add_argument('--labels', dest='labels', nargs='+', default=[],
+                    help='Labels for the training data.')
 
 parser.add_argument('--episodic-files', dest='ep_file_paths', nargs='+', default=[],
                     help='Files which hold the episodic training data.')
-parser.add_argument('--episodic-labels', dest='ep_labels', nargs='+', default=[],
-                    help='Labels for the episodic training data.')
 
 
 args = parser.parse_args()
 
-plt.axhline(y=0, color='r', linestyle='--', label='Zero Line')
+plt.axhline(y=0, color='r', linestyle='--')
 
 cmap = plt.cm.get_cmap('hsv', len(args.avg_file_paths) + 1)
 
 for i in range(len(args.avg_file_paths)):
     colour = cmap(i)
     avg_data = np.load(args.avg_file_paths[i])
-    plt.plot(avg_data, label=args.avg_labels[i], c=colour)
+    plt.plot(avg_data, label=args.labels[i], c=colour)
 
     if len(args.avg_file_paths)==len(args.ep_file_paths):
         ep_data = np.load(args.ep_file_paths[i])
-        plt.plot(ep_data, label=args.ep_labels[i], alpha=0.4, c=colour)
+        plt.plot(ep_data, alpha=0.4, c=colour)
 
 
-plt.legend()
+plt.legend(loc='lower right')
 plt.title('Training Curve')
 plt.xlabel('Episode')
 plt.ylabel('Reward')
